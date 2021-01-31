c6744cf537604fdc55ca9289df4fe0421b6c2f8f
harvp0wn <32920063+harvp0wn@users.noreply.github.com>
2021-01-30 21:30:46 -0800

    Update 1-update-and-fix.sh

--- 1-update-and-fix.sh
+++ 1-update-and-fix.sh
@@ -3,8 +3,12 @@
 # When the system boots for the first time login to the root account (password root) and run this script.
 # It will connect to the internet, update, and then run a fix for the next boot.
 #
+# Variables - Set your prefered DNS Server and wifi-config file name
+DNS=208.67.222.222
+WIFI=wlan0-house
+#
 # Connect to wifi
-netctl start wlan0-<SSID>
+netctl start ${WIFI}
 ### If you did not copy a config file then use wifi-menu to generate one
 ### wifi-menu
 #
@@ -19,13 +23,9 @@ rm /usr/lib/systemd/system/systemd-timesyncd.service
 rm /usr/lib/systemd/system/systemd-resolved.service
 rm /etc/resolvconf.conf
 rm /etc/resolv.conf
-#
-# Change these to your prefered DNS server
-# I personally use a pihole on my local network but I put the OpenDNS server below
 touch /etc/resolv.conf
 echo "nameserver 127.0.0.1" >> /etc/resolv.conf
-echo "nameserver 208.67.222.222" >> /etc/resolv.conf
-#
+echo "nameserver ${DNS}" >> /etc/resolv.conf
 #
 # Need to change a line in this file (if not aleady done at last step)
 read -p "Find ExecStart=systemd-tmpfiles --create --remove --boot --exclude-prefix=/dev"
